713d2b69b7d4f685c9234d4448e48135655a0943
Victor Vermeulen <vcvermeulen@gmail.com>
2023-06-03 08:43:14 +0200

    add morgan logging

---
+++ server/src/models/launches.model.js
@@ -0,0 +1,9 @@
+const launch = {
+	mission: 'Kepler Exploration X',
+	rocket: 'Explorer IS1',
+	launchDate: new Date('December 27, 2030'),
+	target: 'Kepler-442 b',
+	customers: ['ZTM', 'NASA'],
+	upcoming: true,
+	success: true,
+};
