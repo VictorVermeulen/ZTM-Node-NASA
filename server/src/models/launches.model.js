2deb9229e4d86dd18fd7842ebe5cbf2f43c4f164
Victor Vermeulen <vcvermeulen@gmail.com>
2023-06-04 21:24:33 +0200

    add wildcard for html routes

--- server/src/models/launches.model.js
+++ server/src/models/launches.model.js
@@ -1,4 +1,7 @@
+const launches = new Map();
+
 const launch = {
+	flightNumber: 100,
 	mission: 'Kepler Exploration X',
 	rocket: 'Explorer IS1',
 	launchDate: new Date('December 27, 2030'),
@@ -6,4 +9,22 @@ const launch = {
 	customers: ['ZTM', 'NASA'],
 	upcoming: true,
 	success: true,
+};
+
+const launchTwo = {
+	flightNumber: 101,
+	mission: 'Kepler Exploration X',
+	rocket: 'Explorer IS1',
+	launchDate: new Date('December 27, 2030'),
+	target: 'Kepler-442 b',
+	customers: ['ZTM', 'NASA'],
+	upcoming: true,
+	success: true,
+};
+
+launches.set(launch.flightNumber, launch);
+launches.set(launchTwo.flightNumber, launchTwo);
+
+module.exports = {
+	launches,
 };
